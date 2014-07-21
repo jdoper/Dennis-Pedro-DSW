c5953cfb00f1c3b96e65ff7394ad4f16356b3961
Peter Parker <atomas395@gmail.com>
2014-07-21 00:28:58 -0300

    Realizacao da tarefa extra

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -28,7 +28,8 @@ class LineItemsController < ApplicationController
   def create
     product = Product.find(params[:product_id])
     @line_item = @cart.add_product(product.id)
-
+    session[:counter] = 0
+    
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to @line_item.cart}
