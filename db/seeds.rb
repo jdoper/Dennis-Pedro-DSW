590974c233275c5129f95ec7b09a6303f25c7584
jdoper <atomas395@gmail.com>
2014-09-02 23:35:11 -0300

    Inserindo seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,41 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Product.create!(title: 'Programming Ruby 1.9 & 2.0',
+#Product.create!(title: 'Programming Ruby 1.9 & 2.0',
+#	description:
+#		%{<p>
+#			Ruby is the fastest growing and most exciting dynamic language
+#			out there. If you need to get working programs delivered fast,
+#			you should add Ruby to your toolbox.
+#		</p>},
+#	image_url: 'ruby.jpg',
+#	price: 49.95)
+Product.create!(title: 'Git e GitHub: versionamento',
 	description:
 		%{<p>
-			Ruby is the fastest growing and most exciting dynamic language
-			out there. If you need to get working programs delivered fast,
-			you should add Ruby to your toolbox.
+			O Git é uma dessas ferramentas de controle de versão. Foi 
+			criado em 2005 por Linus Torvalds, o criador do Linux, e hoje 
+			é uma das ferramentas de controle de versão mais utilizadas 
+			nos projetos de desenvolvimento de software,
 		</p>},
-	image_url: 'ruby.jpg',
-	price: 49.95)
+	image_url: 'git.jpg',
+	price: 51.80)
+Product.create!(title: 'Node.js JavaScript',
+	description:
+		%{<p>
+			Node.js é uma poderosa plataforma. Ele permite escrever aplicações 
+			JavaScript no server-side, tirando proveito da sintaxe e 
+			familiaridade da linguagem para escrever aplicações web escaláveis.
+		</p>},
+	image_url: 'node.jpg',
+	price: 35.25)
+Product.create!(title: 'UX Design: Boas Praticas',
+	description:
+		%{<p>
+			Atualmente, a experiência do usuário (User Experience – UX) tem 
+			ganhado um papel fundamental na criação de sites e apps e entender 
+			como o usuário se comportará no seu sistema é de extremo valor para 
+			criar um site simples para o usuário.
+		</p>},
+	image_url: 'design.jpg',
+	price: 39.95)
